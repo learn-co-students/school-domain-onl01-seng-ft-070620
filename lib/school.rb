65747eb49c3aaf019ce820c39bbf61b44353fcba
mavrik83 <rjhuber83@gmail.com>
2020-07-14 08:19:17 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+class School
+
+    attr_accessor :name, :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student, level)
+        roster[level] ||= []
+        roster[level] << student
+    end
+
+    def grade(level)
+        roster.detect do |x, y|
+            if x == level
+                return y
+            end
+        end
+    end
+
+    def sort
+        nuhash = {}
+        roster.each do |x, y|
+            nuhash[x] = y.sort
+        end
+        nuhash
+    end
+
+end
