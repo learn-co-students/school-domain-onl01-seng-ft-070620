28e76e03b64a94d7cda39692ddb1a210d8c088d7
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-07-15 00:12:38 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,45 @@
-# code here!
+class School
+  
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+  
+  def school_name=(school_name)
+    @school_name = school_name
+  end 
+  
+  def school_name
+    @school_name
+  end 
+  
+  def roster=(roster)
+    @roster = roster
+  end 
+  
+  def roster
+    @roster
+  end 
+  
+  def add_student(name, grade)
+    if @roster[grade] == nil
+      @roster[grade] = []
+      @roster[grade] << name
+    else 
+      @roster[grade] << name
+    end 
+  end 
+  
+  def grade(grade)
+    @roster[grade]
+  end 
+  
+  def sort 
+    new_roster = {}
+    @roster.collect do |grade, name|
+    new_roster[grade] = name.sort
+   end 
+  new_roster
+  end 
+end 
+
